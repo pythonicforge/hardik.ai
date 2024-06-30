885d93b9a55b11ff72ab7c0fb1d716fe015add30
pythonicforge <pythonicforge@gmail.com>
2024-07-01 00:39:29 +0530

    initial commit 🥳

---
+++ server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/home', methods=["GET"])
+def return_home():
+    return jsonify({"message": "Hello World"}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
