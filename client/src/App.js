e5a6bdbe042cb97bc9470b89171bb35806eb5b5c
pythonicforge <pythonicforge@gmail.com>
2024-07-02 01:46:21 +0530

    added footer ✅

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./styles/App.scss";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Body />
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../styles/Footer.scss";
+import QRCODE from "../images/qrcode.png";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-content section-gap">
+        <div className="row-1">
+          <p>Stay in touch.</p>
+          <img src={QRCODE} alt="" />
+        </div>
+        <div className="line"></div>
+        <div className="footer-end-container">
+          <div className="footer-links">
+            <a
+              href="https://linkedin.com/in/pseudopythonic"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://twitter.com/hardeadik"
+              target="_blank"
+              rel="noreferrer"
+            >
+              X
+            </a>
+            <a
+              href="https://github.com/pythonicforge"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="mailto:pythonicforge@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Mail
+            </a>
+          </div>
+          <div className="footer-copyright">
+            <p>© 2024 <span>Hardik Jaiswal</span></p>
+            <p>DESIGNED AND DEVELOPED BY HARDIK JAISWAL</p>
+            <p>CODE FLOWS, GLOWS, CRAFTING DREAMS IN ROWS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
